6adb3c6b53571669c63f3926d9d3f8dcf4be182d
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-01-24 18:29:41 -0300

    #5-release: ajustando formatação e  container docker

--- ch5/banco_nao_relacional.py
+++ ch5/banco_nao_relacional.py
@@ -2,18 +2,20 @@ import pymongo
 
 
 def manipula_dados():
-    cliente = pymongo.MongoClient("mongodb://192.168.56.1:27017")
+    cliente = pymongo.MongoClient("mongodb://172.26.150.168:27017")
     db = cliente.conheca_python
 
     for i in range(1, 10):
         obj_dic = {"codigo": i}
         db.conheca_mongo.insert_one(obj_dic)
 
-    res_con = db.conheca_mongo.find({})
+    res_con = db.conheca_mongo.find()
+    db.conheca_mongo.delete_one({"codigo": 9})
+    db.conheca_mongo.update_one(
+        {"codigo": 2}, {"$set": {"atributo_novo": 789}})
     for doc in res_con:
         print(doc)
-    db.conheca_mongo.delete_one({"codigo": 1})
-    db.conheca_mongo.update({"codigo": 2}, {"$set": {"atributo_novo": 789}})
+    print("[INFO] - Manipulação concluída!")
 
 
 def main():

8542d3fcc0d11bef1679a1cc429660e6855d46c5
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-01-24 04:15:04 -0300

    #4-release: implementando manipulação de xml e tipagem em funções

--- ch4/manipula_html.py
+++ ch4/manipula_html.py
@@ -2,12 +2,12 @@ from html.parser import HTMLParser
 from datetime import datetime
 
 class myParser(HTMLParser):
-    def write_response(self, response):
+    def write_response(self, response:str) -> None:
         with open("response.txt", "a+") as f: 
             f.write(response)
         f.close()
         
-    def handle_starttag(self, tag, attrs):
+    def handle_starttag(self, tag:str, attrs:list) -> None:
         if tag == "title":
             hj = datetime.now()
             self.write_response(f"{hj.strftime('%c')}\n")
@@ -16,15 +16,15 @@ class myParser(HTMLParser):
         elif tag=="img" and attrs.__len__() > 0:
             self.write_response(f"{tag}-N/A => {attrs[0][1]}\n")
         
-    def handle_comment(self,data):
+    def handle_comment(self,data:str) -> None:
         print(f"comentario: {data}")
 
-    def handle_endtag(self, tag: str):
+    def handle_endtag(self, tag: str)  -> None:
         if tag == "html": 
             self.write_response("\n")
             print("[INFO] - Dados salvos em response.txt!")
         
-    def handle_data(self, data):
+    def handle_data(self, data) -> None:
         return
                   
 
